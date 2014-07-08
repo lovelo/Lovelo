bf8ad12995f8801f89e82f0d9567578f7b1d7c1c
Lovelo <lovelofitlab@gmail.com>
2014-07-07 22:38:11 -0300

    Alterando os pacotes

--- WebContent/index.jsp
+++ WebContent/index.jsp
@@ -6,11 +6,11 @@
 <html>
 <head>
     <meta http-equiv="Content-type" content="text/html; charset=utf-8">
-    <title>SaleService</title>
+    <title>Lovelo</title>
     <link rel="stylesheet" href="styles/styles.css" type="text/css" media="screen">
 </head>
 <body>
-	<s:form action="insertUser">
+	<s:form action="loginUser">
 <%-- 		<s:push value="user"> --%>
 			<s:textfield name="user.user" label="Usuário" />
 			<s:textfield name="user.password" label="Password" />
--- src/br/com/lovelo/actions/UserAction.java
+++ src/br/com/lovelo/actions/UserAction.java
@@ -1,9 +1,5 @@
 package br.com.lovelo.actions;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-
 import br.com.lovelo.controller.UserController;
 import br.com.lovelo.model.User;
 
@@ -18,36 +14,10 @@ public class UserAction extends ActionSupport{
 	private UserController userController = new UserController();
 	
 	
-	
-    public static void main(String[] args) {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("lovelo");
-        EntityManager em = emf.createEntityManager();
- 
-        try {
-            em.getTransaction().begin();
-             
-            User user = new User();
-//            user.setName("abc123");
-            user.setPassword("abc123");
-             
-            em.persist(user);
-             
-            em.getTransaction().commit();
-        }
-        catch (Exception e) {
-            em.getTransaction().rollback();
-            e.printStackTrace();
-        }
-        finally{
-            emf.close();
-        }
-         
-        System.out.println("It is over");
-    }
 	public String login(){
 		return "success";
 	}
-	public String insertUser(){
+	public String loginUser(){
 		userController.save(user);
 		return SUCCESS;
 	}
